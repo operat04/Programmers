28f9eb081c5c5a1110a933b7385a5ae14c8c0cac
kyusung <111227582+operat04@users.noreply.github.com>
2023-04-06 15:54:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,19 @@
+function solution(num) {
+    let count = 0
+    if(num === 1){
+        return 0
+    }
+    while(num>1){
+        if(count >= 500){
+            return -1
+        }
+        if(num%2 === 0){
+            num = num/2;
+            count +=1;
+        }else if(num%2 === 1){
+            num = num*3 +1;
+            count +=1;
+        }
+    }
+    return count;
+}
