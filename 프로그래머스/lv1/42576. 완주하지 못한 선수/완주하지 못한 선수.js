1c1e246fcc1dcbbce08e80c62b241a163389f552
kyusung <111227582+operat04@users.noreply.github.com>
2023-05-11 23:09:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 122.98 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,13 +1,21 @@
-function solution(participant, completion) {
-    let result = "";
-    participant.sort();
-    completion.sort();
+// function solution(participant, completion) {
+//     let result = "";
+//     participant.sort();
+//     completion.sort();
     
-    for(let i=0; i<participant.length; i++){
-        if(participant[i] !== completion[i]){
-            result = participant[i];
-            break;
-        }
+//     for(let i=0; i<participant.length; i++){
+//         if(participant[i] !== completion[i]){
+//             result = participant[i];
+//             break;
+//         }
+//     }
+//     return result;
+// }
+const solution = (p, c) => {
+    p.sort()
+    c.sort()
+    while (p.length) {
+        let pp = p.pop()
+        if (pp !== c.pop()) return pp
     }
-    return result;
 }
